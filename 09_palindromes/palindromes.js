aa14e9bce5e9d3c40a6bfe8d5c6fb968da3f27e4
Aquibul Haq Chowdhury <aquibulhaqchowdhury@gmail.com>
2024-10-23 00:41:08 +0600

    feat(palindromes): implement palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+// function isAlnum(c) {
+//   return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9';
+// }
 
+// const palindromes = function (str) {
+//   for (let i = 0, j = str.length - 1; i < j; ++i, --j) {
+//     while (i < j && !isAlnum(str[i]))
+//       ++i;
+//     while (i < j && !isAlnum(str[j]))
+//       --j;
+
+//     if (str[i].toLowerCase() !== str[j].toLowerCase())
+//       return false;
+//   }
+
+//   return true;
+// };
+
+const palindromes = function (str) {
+  const cleaned = str.toLowerCase().replaceAll(/[^a-z0-9]+/g, '');
+  const reversed = cleaned.split('').reverse().join('');
+  return cleaned === reversed;
 };
 
 // Do not edit below this line
